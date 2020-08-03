4cc04638ff8f9818628e14dd7d1a846970d3c25d
GustavoOM <gumartins2001@gmail.com>
2020-08-03 07:52:35 -0300

    :sparkles: Insert a nodemailer

--- backend/src/modules/users/services/ResetPasswordService.ts
+++ backend/src/modules/users/services/ResetPasswordService.ts
@@ -1,14 +1,16 @@
 import { injectable, inject } from 'tsyringe';
+import { isAfter, addHours } from "date-fns"
 
 import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 //import User from '../infra/typeorm/entities/User';
 
 
 interface IUserRequest {
-    token: string;
+  token: string;
   password: string;
 }
 
@@ -21,6 +23,8 @@ class ResetPasswordService {
     @inject("UserTokensRepository")
     private userTokensRepository: IUserTokensRepository,
 
+    @inject("HashProvider")
+    private hashProvider: IHashProvider,
 
   ) {}
 
@@ -37,7 +41,15 @@ class ResetPasswordService {
         throw new AppError("User does not exists")
       }
 
-      user.password = password
+      const tokenCreatedAt = userToken.created_at
+
+      const compareDate = addHours(tokenCreatedAt,2)
+
+      if(isAfter(Date.now(), compareDate)){
+        throw new AppError("Token expired")
+      }
+
+      user.password = await this.hashProvider.generateHash(password)
 
       await this.usersRepository.update(user)
   }
--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -9,8 +9,8 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-//import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-//import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 
 
@@ -23,4 +23,9 @@ container.registerSingleton<IAppointmentRepository>(
 container.registerSingleton<IUsersRepository>(
   'UserRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
